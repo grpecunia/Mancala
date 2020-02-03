8b61f752c54474bba336171b5e749f7bc7c71304
Nate Falconer <falconerresources@gmail.com>
2020-02-03 15:59:09 -0500

    added click boxes and changes some css

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
   document.querySelector('#start-button').onclick = function() {
       this.remove()  //removes start button
       document.querySelector("body > div").remove()
+      document.querySelector("header > h1").remove()
       startGame() //calls startGame
     }
 
 const canvas = document.querySelector('#canvas'); //Get the canvas
 var img = new Image(); //load an image element
 
-canvas.width = window.innerWidth - 15;
-canvas.height = window.innerHeight - 20;
+canvas.width = 900;
+canvas.height = 500;
 
-// document.querySelector('#start-button').click();
+document.querySelector('#start-button').click();
 
 const ctx = canvas.getContext('2d');
 
@@ -32,22 +33,73 @@ let mancalaBoard = [
 var activePlayer = 0;
 
   function drawBoard() {
-    ctx.fillStyle = 'green'
+    ctx.fillStyle = 'none';
     ctx.fillRect(0,0,canvas.width, canvas.height) //draws the green grass 
   }
 
   let board = {
-    x: canvas.width / 2 - 500,
-    y: canvas.height / 2 - 150,
-    width: 1000,
+    x: 75,
+    y: 150,
+    width: 750,
     height: 300
   };
 
   function drawBoard() {
     ctx.drawImage(img, board.x, board.y, board.width, board.height); //draws the board depending on the coords in the obj above 
     mancalaBoard = [[4, 4, 4, 4, 4, 4, 0], [4, 4, 4, 4, 4, 4, 0]]
+    
 
   }
+
+  // function drawSquares() {
+  // ctx.rect(180, 185, 75, 100);
+  // ctx.rect(275, 185, 75, 100);
+  // ctx.rect(370, 185, 75, 100);
+  // ctx.rect(470, 185, 75, 100);
+  // ctx.rect(560, 185, 75, 100);
+  // ctx.rect(650, 185, 75, 100);
+  // ctx.rect(185, 315, 75, 100);
+  // ctx.rect(275, 315, 75, 100);
+  // ctx.rect(365, 315, 75, 100);
+  // ctx.rect(465, 315, 75, 100);
+  // ctx.rect(555, 315, 75, 100);
+  // ctx.rect(650, 315, 75, 100);
+  // ctx.rect(85, 190, 80, 220);//mancala 1 location
+  // ctx.rect(740, 190, 80, 220);// mancala 2 location
+  // ctx.stroke();
+  // }
+
+  document.onclick = boxClick
+
+  function boxClick(e) {
+    if (e.x >= 180 && e.x <= 255 && e.y >= 185 && e.y <= 285)
+    console.log('you clicked box12!');
+    if (e.x >= 275 && e.x <= 350 && e.y >= 185 && e.y <= 285)
+    console.log('you clicked box11!');
+    if (e.x >= 370 && e.x <= 445 && e.y >= 185 && e.y <= 285)
+    console.log('you clicked box10!');
+    if (e.x >= 470 && e.x <= 545 && e.y >= 185 && e.y <= 285)
+    console.log('you clicked box9!');
+    if (e.x >= 560 && e.x <= 635 && e.y >= 185 && e.y <= 285)
+    console.log('you clicked box8!');
+    if (e.x >= 650 && e.x <= 725 && e.y >= 185 && e.y <= 285)
+    console.log('you clicked box7!');
+    if (e.x >= 185 && e.x <= 260 && e.y >= 315 && e.y <= 415)
+    console.log('you clicked box0!');
+    if (e.x >= 275 && e.x <= 350 && e.y >= 315 && e.y <= 415)
+    console.log('you clicked box1!');
+    if (e.x >= 365 && e.x <= 440 && e.y >= 315 && e.y <= 415)
+    console.log('you clicked box2!');
+    if (e.x >= 465 && e.x <= 540 && e.y >= 315 && e.y <= 415)
+    console.log('you clicked box3!');
+    if (e.x >= 555 && e.x <= 630 && e.y >= 315 && e.y <= 415)
+    console.log('you clicked box4!');
+    if (e.x >= 650 && e.x <= 725 && e.y >= 315 && e.y <= 415)
+    console.log('you clicked box5!');
+    }
+  
+  
+  
 
   function playerMove() {
       
@@ -58,4 +110,5 @@ var activePlayer = 0;
     
     ctx.clearRect(0,0,canvas.width, canvas.height) //clears the whole canvas, the car, the board everything in the canvas
     drawBoard()  //redraws the board over and over and over again
+    // drawSquares()
   }
