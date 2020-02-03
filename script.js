9d9ba02911bcc6041dbcd490795ee832980a82d5
Gustavo J Rivera Pecunia <grpecunia@Gustavos-MacBook-Pro.local>
2020-02-03 17:38:30 -0500

    'changes to include bootstrap'

--- script.js
+++ script.js
@@ -9,8 +9,11 @@
 const canvas = document.querySelector('#canvas'); //Get the canvas
 var img = new Image(); //load an image element
 
-canvas.width = window.innerWidth - 15; //MAC
-canvas.height = window.innerHeight - 20; //MAC
+// canvas.width = window.innerWidth - 15; //MAC
+// canvas.height = window.innerHeight - 20; //MAC
+
+// canvas.width = 900; //PC
+// canvas.height = 500; //PC
 
 document.querySelector('#start-button').click();
 
@@ -53,20 +56,51 @@ var activePlayer = 0;
   }
 
   function grab (i) {
-      document.onclick = function (e) {
-          
-      }
-      console.log(mancalaBoard)
-      mancalaBoard[i] = 0
-      mancalaBoard[i+1]++
-      mancalaBoard[i+2]++
-      mancalaBoard[i+3]++
-      mancalaBoard[i+4]++
-      console.log(mancalaBoard, i)
+      let value;
+        switch (i){
+            case 0: 
+                mancalaBoard[0] = value;
+                mancalaBoard[0] = 0;
+                for (let i = 0; i < value; i++) {
+                    mancalaBoard[i]++
+                } 
+                console.log(value)
+                break;
+            case 1:
+                mancalaBoard[1] = value;
+                mancalaBoard[1] = 0;
+                for (let i = 0; i < value; i++) {
+                    mancalaBoard[i]++
+                } 
+                break;
+            }  
+    console.log(mancalaBoard) 
+    }
+    
+    
+    
+    // document.onclick = function (e) {   
+        
+    // }
+    //   console.log(mancalaBoard)
+    //   mancalaBoard[i] = 0
+    //   mancalaBoard[i+1]++
+    //   mancalaBoard[i+2]++
+    //   mancalaBoard[i+3]++
+    //   mancalaBoard[i+4]++
+    //   mancalaBoard[i+5]++;
+    //   mancalaBoard[i+6]++;
+    //   mancalaBoard[i+7]++;
+    //   mancalaBoard[i+8]++;
+    //   mancalaBoard[i+9]++;
+    //   mancalaBoard[i+10]++;
+    //   mancalaBoard[i+11]++;
+    //   mancalaBoard[i+12]++;
+    //   console.log(mancalaBoard, i)
     //   document.onclick = function (e) {
     //       console.log(e)
     //   }
-  }
+  //}
 
   // function drawSquares() {
   // ctx.rect(180, 185, 75, 100);
@@ -90,29 +124,29 @@ var activePlayer = 0;
 
   function boxClick(e) {
     if (e.x >= 180 && e.x <= 255 && e.y >= 185 && e.y <= 285)
-    console.log('you clicked box12!');
+    grab(12);
     if (e.x >= 275 && e.x <= 350 && e.y >= 185 && e.y <= 285)
-    console.log('you clicked box11!');
+    grab(11)
     if (e.x >= 370 && e.x <= 445 && e.y >= 185 && e.y <= 285)
-    console.log('you clicked box10!');
+    grab(10)
     if (e.x >= 470 && e.x <= 545 && e.y >= 185 && e.y <= 285)
-    console.log('you clicked box9!');
+    grab(9)
     if (e.x >= 560 && e.x <= 635 && e.y >= 185 && e.y <= 285)
-    console.log('you clicked box8!');
+    grab(8)
     if (e.x >= 650 && e.x <= 725 && e.y >= 185 && e.y <= 285)
-    console.log('you clicked box7!');
+    grab(7)
     if (e.x >= 185 && e.x <= 260 && e.y >= 315 && e.y <= 415)
-    console.log('you clicked box0!');
+    grab(0)
     if (e.x >= 275 && e.x <= 350 && e.y >= 315 && e.y <= 415)
-    console.log('you clicked box1!');
+    grab(1)
     if (e.x >= 365 && e.x <= 440 && e.y >= 315 && e.y <= 415)
-    console.log('you clicked box2!');
+    grab(2)
     if (e.x >= 465 && e.x <= 540 && e.y >= 315 && e.y <= 415)
-    console.log('you clicked box3!');
+    grab(3)
     if (e.x >= 555 && e.x <= 630 && e.y >= 315 && e.y <= 415)
-    console.log('you clicked box4!');
+    grab(4)
     if (e.x >= 650 && e.x <= 725 && e.y >= 315 && e.y <= 415)
-    console.log('you clicked box5!');
+    grab(5)
     }
 
 
