2ddfbcdda9887edfe4d14dab444cf7ff476d2e25
Gustavo J Rivera Pecunia <grpecunia@Gustavos-MacBook-Pro.local>
2020-02-04 16:33:28 -0500

    Changes to the main.js

--- main.js
+++ main.js
@@ -1,136 +1,122 @@
-var mancalaBoard, activePlayer, score, stoneCount;
+var mancalaBoard, activePlayer, score, stoneCount, conditional, gamePlaying;
 
 function init() {
-    mancalaBoard = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
-    console.log(mancalaBoard);
-    score = [mancalaBoard[6],mancalaBoard[13]];
-    activePlayer = 0;
-    document.querySelector("#start-button").remove();
-    document.querySelector("body > header > div.rules").remove();
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(1)"
-    ).classList.add('active')
-    updateMancala();
-    console.log("Start");
+  gamePlaying = true;
+  mancalaBoard = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
+  console.log(mancalaBoard);
+  score = [mancalaBoard[6], mancalaBoard[13]];
+  activePlayer = 0;
+  document.querySelector("#start-button").remove();
+  document.querySelector("body > header > div.rules").remove();
+  document
+    .querySelector("body > div.container > div > div.col > div:nth-child(1)")
+    .classList.add("active");
+  updateMancala();
+  console.log("Start");
 }
 
-document.querySelector("#start-button").addEventListener('click', init)
+document.querySelector("#start-button").addEventListener("click", init);
 
 function move(loc) {
-  document.onclick = function(e) {
+    document.querySelector( "div.row.active").onclick = function(e) {
     let stoneCount = mancalaBoard[Number(e.target.attributes.id.value)];
     let conditional = Number(e.target.attributes.id.value) + stoneCount;
-      for (
-        let i = Number(e.target.attributes.id.value); i <= Number(e.target.attributes.id.value) + stoneCount; i++) {
-        console.log("i: ", i);
-        console.log("index: ", Number(e.target.attributes.id.value));
-        console.log("stoneCount: ", stoneCount);
-        console.log(
-          "conditional: ",
-          Number(e.target.attributes.id.value) + stoneCount
-        );
-        mancalaBoard[i%14]++;
-      }
+    let position;
+    for (let i = Number(e.target.attributes.id.value); i <= Number(e.target.attributes.id.value) + stoneCount; i++) {
+        mancalaBoard[i % 14]++;
+        position = mancalaBoard[conditional]
+      console.log('>>>>COND>>> '+conditional, '>>>>POS>>> '+position);
+    }
+    console.log('Position Value: ' + position);
+    console.log('Value at Bucket: ' + mancalaBoard[6]);
     
+    if (activePlayer === 0 && position === mancalaBoard[6]) {
+      activePlayer = 0;
+    } else if (activePlayer === 0 && position !== mancalaBoard[6]) {
+      document
+        .querySelector("div.row.pit-" + activePlayer)
+        .classList.remove("active");
+      activePlayer = 1;
+      document
+        .querySelector("div.row.pit-" + activePlayer)
+        .classList.add("active");
+    } else if (activePlayer === 1 && position === mancalaBoard[13]) {
+      activePlayer = 1;
+    } else if (activePlayer === 1 && position !== mancalaBoard[13]) {
+      document
+        .querySelector("div.row.pit-" + activePlayer)
+        .classList.remove("active");
+      activePlayer = 0;
+      document
+        .querySelector("div.row.pit-" + activePlayer)
+        .classList.add("active");
+    } else {
+        console.log('sometingwong!!')
+    }
     mancalaBoard[e.target.attributes.id.value] = 0;
-    //   console.log(e.target.attributes.id.value, stoneCount, mancalaBoard[e.target.attributes.id.value]);
     updateMancala();
-    console.log(mancalaBoard);
   };
 }
 
-// function move(loc) {
-//     document.onclick = function(e) {
-//         console.log(e)
-//         let stoneCount = mancalaBoard[Number(e.target.attributes.id.value)];
-//         let conditional = Number(e.target.attributes.id.value) + stoneCount;
-//             for (let i = Number(e.target.attributes.id.value); i <= conditional; i++) {
-//                 mancalaBoard[i%14]++
-//                 }
-//             } 
-//         mancalaBoard[e.target.attributes.id] = 0;
-//       updateMancala();  
-//     //   console.log(mancalaBoard);
-//     };
-
-// function move(loc) {
-//   document.onclick = function(e) {
-//     let stoneCount = mancalaBoard[Number(e.target.attributes.id.value)];
-//     let conditional = Number(e.target.attributes.id.value) + stoneCount;
-//     if (conditional <= 13) {
-//       for (
-//         let i = Number(e.target.attributes.id.value);
-//         i <= Number(e.target.attributes.id.value) + stoneCount;
-//         i++
-//       ) {
-//         console.log("i: ", i);
-//         console.log("index: ", Number(e.target.attributes.id.value));
-//         console.log("stoneCount: ", stoneCount);
-//         console.log(
-//           "conditional: ",
-//           Number(e.target.attributes.id.value) + stoneCount
-//         );
-//         mancalaBoard[i]++;
-//       }
-//     } else {
-//     }
-//     mancalaBoard[e.target.attributes.id.value] = 0;
-//     //   console.log(e.target.attributes.id.value, stoneCount, mancalaBoard[e.target.attributes.id.value]);
-//     updateMancala();
-//     console.log(mancalaBoard);
-//   };
-// }
-
 function updateMancala() {
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(6)"
-    ).innerHTML = mancalaBoard[0];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(5)"
-    ).innerHTML = mancalaBoard[1];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(4)"
-    ).innerHTML = mancalaBoard[2];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(3)"
-    ).innerHTML = mancalaBoard[3];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(2)"
-    ).innerHTML = mancalaBoard[4];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(1)"
-    ).innerHTML = mancalaBoard[5];
-    document.querySelector("#\\36 ").innerText = mancalaBoard[6];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(1)"
-    ).innerHTML = mancalaBoard[7];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(2)"
-    ).innerHTML = mancalaBoard[8];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(3)"
-    ).innerHTML = mancalaBoard[9];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(4)"
-    ).innerHTML = mancalaBoard[10];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(5)"
-    ).innerHTML = mancalaBoard[11];
-    document.querySelector(
-      "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(6)"
-    ).innerHTML = mancalaBoard[12];
-    document.querySelector("#\\31 3").innerText = mancalaBoard[13];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(6)"
+  ).innerHTML = mancalaBoard[0];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(5)"
+  ).innerHTML = mancalaBoard[1];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(4)"
+  ).innerHTML = mancalaBoard[2];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(3)"
+  ).innerHTML = mancalaBoard[3];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(2)"
+  ).innerHTML = mancalaBoard[4];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(1) > div:nth-child(1)"
+  ).innerHTML = mancalaBoard[5];
+  document.querySelector("#\\36 ").innerText = mancalaBoard[6];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(1)"
+  ).innerHTML = mancalaBoard[7];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(2)"
+  ).innerHTML = mancalaBoard[8];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(3)"
+  ).innerHTML = mancalaBoard[9];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(4)"
+  ).innerHTML = mancalaBoard[10];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(5)"
+  ).innerHTML = mancalaBoard[11];
+  document.querySelector(
+    "body > div.container > div > div.col > div:nth-child(2) > div:nth-child(6)"
+  ).innerHTML = mancalaBoard[12];
+  document.querySelector("#\\31 3").innerText = mancalaBoard[13];
 }
 
-
 //****   Functionality to increase and decrease Stone Count in the NavBar Modal  */
-document.querySelector("#stones > div > div > div.modal-body > button.btn.btn-outline-success").addEventListener('click', upStone)
-document.querySelector("#stones > div > div > div.modal-body > button.btn.btn-outline-danger").addEventListener('click', downStone)
+document
+  .querySelector(
+    "#stones > div > div > div.modal-body > button.btn.btn-outline-success"
+  )
+  .addEventListener("click", upStone);
+document
+  .querySelector(
+    "#stones > div > div > div.modal-body > button.btn.btn-outline-danger"
+  )
+  .addEventListener("click", downStone);
 
 function upStone() {
-    document.querySelector("#stones > div > div > div.modal-body > input").value++;
+  document.querySelector("#stones > div > div > div.modal-body > input")
+    .value++;
 }
 
 function downStone() {
-    document.querySelector("#stones > div > div > div.modal-body > input").value--;
+  document.querySelector("#stones > div > div > div.modal-body > input")
+    .value--;
 }
